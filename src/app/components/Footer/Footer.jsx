3527c14989da2a94151c1c3619ed2d783634ee5a
alaminislam34 <mi3548514@gmail.com>
2025-09-16 06:30:13 +0600

    added footer design

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,11 +1,26 @@
-'use client'
+"use client";
 
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="bg-[#2A2A2A]">
+      <footer className="max-w-[1440px] w-11/12 mx-auto">
+        <div>
+          <Image src={"/logo.png"} height={200} width={200} alt="Logo icon" className="lg:w-[122px] lg:h-[74px] mb-7" />
+          <p className="text-[#ADADAD] text-sm md:text-lg lg:text-xl">
+            TaskMama is the all-in-one app for mompreneurs — combining family
+            calendars, chores, meal planning, and business tools with AI Daily
+            Balance to keep life organized and stress-free.
+          </p>
+        </div>
+        <div>
+          <h1 className="text-sm md:text-lg lg:text-xl font-medium">Download it now</h1>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
