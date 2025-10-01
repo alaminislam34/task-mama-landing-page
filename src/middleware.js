8caf3fd128b4d37aeb6b81ab2454dbdbff7cf969
alaminislam34 <mi3548514@gmail.com>
2025-10-01 23:46:00 +0600

    added home page title changes

--- src/middleware.js
+++ src/middleware.js
@@ -1,20 +1,17 @@
 // /middleware.js
 import { NextResponse } from "next/server";
-import { jwtVerify } from "jose"; // jwtVerify ব্যবহার করলে Next.js Edge Compatible হয়
+import { jwtVerify } from "jose";
 
 export async function middleware(req) {
   const token = req.cookies.get("token")?.value;
 
-  // যদি token না থাকে → login page এ redirect
   if (!token) {
     return NextResponse.redirect(new URL("/signin", req.url));
   }
 
   try {
-    // JWT verify (Edge friendly)
     await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET));
 
-    // Valid token → next()
     return NextResponse.next();
   } catch (err) {
     console.error("Middleware token error:", err.message);
@@ -22,7 +19,6 @@ export async function middleware(req) {
   }
 }
 
-// Middleware configuration: কোন routes এ লাগবে
 export const config = {
-  matcher: ["/coursepanel/:path*"], // /coursepanel এবং তার subroutes
+  matcher: ["/coursepanel/:path*"],
 };
